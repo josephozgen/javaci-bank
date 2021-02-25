5b142a1ec9ac89943ed256e14cb490541b3268be
Yusuf Ozgen <mahmutaltun@Yusuf-iMac.local>
2021-02-24 23:09:23 -0800

    Javaci Bank Back office initial commit

---
+++ src/main/java/net/javaci/bank/JavaciBankBackofficeApplication.java
@@ -0,0 +1,13 @@
+package net.javaci.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaciBankBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaciBankBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaci/bank/JavaciBankBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaci.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaciBankBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
